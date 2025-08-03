58030b2e7d83ac09545b3d43c42d9ec4e5d949c4
Habtamu Asayto <habtamuasayto360@example.com>
2025-08-03 19:44:48 +0300

    Week 4 commit

--- week_3.py
+++ week_3.py
@@ -1,3 +1,4 @@
+'''
 def  calculate_discount(price, discount_percent) :
     if discount_percent >= (20/100)* price:
         discount_percent = (20/100) * price
@@ -9,3 +10,10 @@ price = float(input("Enter the price: "))
 discount = float(input("Enter the discount percent: "))
 new_price = calculate_discount(price, discount)
 print(f"The new price after discount is: {new_price}")
+'''
+try:
+    file = open("input.txt", "a")
+    file.write("\nThis is a new line added to the file.\n")
+    print("Append operation successful.")
+except FileNotFoundError:
+    print("Sorry, the file does not exist.")
---
+++ week_4.py
@@ -0,0 +1,40 @@
+
+try:
+    #File Read & Write Challenge 🖋️: Create a program that reads a file and writes a modified version to a new file.
+    #Read the contents of input.txt.
+    file =open("input.txt", "r")
+    contents = file.read()
+    print(contents)
+    #modify to new file
+    new_file = open("output.txt", "w")
+    new_file.write(contents)
+    print("File created successfully.")
+    new_file.close()
+except FileNotFoundError:
+    print("The file input.txt does not exist.")
+
+
+
+
+
+'''
+try:
+    #Read the contents of input.txt.
+    file = open("input.txt", "r")
+    contents = file.read()
+    #Count the number of words in the file.
+    count = len(contents.split())
+    print(f"Word Count: {count}")
+    #Convert all text to uppercase.
+    convert = contents.upper()
+    print("Upper : ",convert)
+    #Write the processed text and the word count to a new file called output.txt
+    output_file = open("output.txt", "w")
+    output_file.write(f"Word Count: {count}\n")
+    #Print a success message once the new file is created.
+    output_file.write(convert)
+    print("File created successfully.")
+except FileNotFoundError:
+    print("The file input.txt does not exist.")
+
+'''
